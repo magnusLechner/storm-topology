5768ff3c1961125fddb22cb278c898e92364ee82
Magnus <magnus.lechner@stud-mail.uni-wuerzburg.de>
2016-04-18 20:07:40 +0200

    working word-count-topology in only JAVA

--- src/main/java/test/WordCountTopology.java
+++ src/main/java/test/WordCountTopology.java
@@ -1,86 +1,55 @@
 package test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
-import org.apache.storm.task.ShellBolt;
-import org.apache.storm.topology.BasicOutputCollector;
-import org.apache.storm.topology.IRichBolt;
-import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.TopologyBuilder;
-import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Fields;
-import org.apache.storm.tuple.Tuple;
-import org.apache.storm.tuple.Values;
 
 public class WordCountTopology {
 
-	@SuppressWarnings("serial")
-	public static class SplitSentence extends ShellBolt implements IRichBolt {
-
-		public SplitSentence() {
-			super("python", "splitsentence.py");
-		}
-
-		@Override
-		public void declareOutputFields(OutputFieldsDeclarer declarer) {
-			declarer.declare(new Fields("word"));
-		}
-
-		@Override
-		public Map<String, Object> getComponentConfiguration() {
-			return null;
-		}
-	}
-
-	@SuppressWarnings("serial")
-	public static class WordCount extends BaseBasicBolt {
-		Map<String, Integer> counts = new HashMap<String, Integer>();
-
-		@Override
-		public void execute(Tuple tuple, BasicOutputCollector collector) {
-			String word = tuple.getString(0);
-			Integer count = counts.get(word);
-			if (count == null)
-				count = 0;
-			count++;
-			counts.put(word, count);
-			collector.emit(new Values(word, count));
-		}
-
-		@Override
-		public void declareOutputFields(OutputFieldsDeclarer declarer) {
-			declarer.declare(new Fields("word", "count"));
-		}
-	}
-
+	// Entry point for the topology
 	public static void main(String[] args) throws Exception {
-		
+		// Used to build the topology
 		TopologyBuilder builder = new TopologyBuilder();
-
+		// Add the spout, with a name of 'spout'
+		// and parallelism hint of 5 executors
 		builder.setSpout("spout", new RandomSentenceSpout(), 5);
-
+		// Add the SplitSentence bolt, with a name of 'split'
+		// and parallelism hint of 8 executors
+		// shufflegrouping subscribes to the spout, and equally distributes
+		// tuples (sentences) across instances of the SplitSentence bolt
 		builder.setBolt("split", new SplitSentence(), 8).shuffleGrouping("spout");
+		// Add the counter, with a name of 'count'
+		// and parallelism hint of 12 executors
+		// fieldsgrouping subscribes to the split bolt, and
+		// ensures that the same word is sent to the same instance (group by
+		// field 'word')
 		builder.setBolt("count", new WordCount(), 12).fieldsGrouping("split", new Fields("word"));
 
+		// new configuration
 		Config conf = new Config();
 		conf.setDebug(true);
 
+		// If there are arguments, we are running on a cluster
 		if (args != null && args.length > 0) {
+			// parallelism hint to set the number of workers
 			conf.setNumWorkers(3);
-
+			// submit the topology
 			StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
-		} else {
+		}
+		// Otherwise, we are running locally
+		else {
+			// Cap the maximum number of executors that can be spawned
+			// for a component to 3
 			conf.setMaxTaskParallelism(3);
-
+			// LocalCluster is used to run locally
 			LocalCluster cluster = new LocalCluster();
+			// submit the topology
 			cluster.submitTopology("word-count", conf, builder.createTopology());
-
-			Thread.sleep(10000);
-
+			// sleep
+			Thread.sleep(10000);			
+			// shut down the cluster
 			cluster.shutdown();
 		}
 	}
